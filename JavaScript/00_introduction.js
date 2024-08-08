acf3fd41462ad8457ecb22113d8501b673655334
priyank <priyankmali297@gmail.com>
2024-08-08 23:44:56 +0530

    javascript

---
+++ JavaScript/00_introduction.js
@@ -0,0 +1,57 @@
+/**
+ * 
+ * 
+-> JavaScript is the most popular scripting language for the Web.
+-> It is easy to learn, lightweight, cross-platform, single-threaded, and interpreted compiled language. 
+-> It is widely used for web development, both on the client side and server side. 
+-> As a scripting language, it plays an important role in web browsers, allowing real-time 
+   modification of web page content and enhancing user experience.
+
+Client-side: 
+-> It supplies objects to control a browser and its Document Object Model (DOM). 
+   Like if client-side extensions allow an application to place elements on an HTML form and 
+   respond to user events such as mouse clicks, form input, and page navigation. 
+   Useful libraries for the client side are:  AngularJS / ReactJS / VueJS, and so many others.
+
+Server-side: 
+-> It supplies objects relevant to running JavaScript on a server. 
+   For if the server-side extensions allow an application to communicate with a database, 
+   and provide continuity of information from one invocation to another of the application, 
+   or perform file manipulations on a server. 
+   The useful framework which is the most famous these days is node.js.
+
+JS v/s NodeJS
+JS(javascript)
+--> javascript is a Dynamically type scripting language for web.
+--> it is single threade programming language.
+--> Asynchronous 
+
+what is Node.Js ?
+--> it is runtime for javascript
+--> javascript ne execute karva mate ek environment joye (same as any other language)
+--> as a human we need a perfect environment to live(H20 , good air , proper temprature , water , food ...)
+--> which allows javascript to run on computer(je javascript ne samaje) thats why we need nodejs.
+
+application of javascript:
+--> Front-end 
+--> Back-end
+--> game development
+--> machine learnnig
+--> IOT(machine to machine comunication)
+--> mobile app development
+
+Life cycle : child             --> we do same to learn computer language(javascript / java / python ...)
+
+1. born                        --> install 
+2. name                        --> veriables       
+3. aware about family type     --> data types
+4. comparision                 --> operaters (arithmatic / ternary / ....)
+5. conditionals                --> coditional (if / else)
+6. repitative things           --> loops (for / while / do-while)            
+7. reuse things                --> functions
+8. interaction with real world --> OOPs
+9. mistake                     --> exception handling
+
+*/
+
+console.log("Hello, World!")
