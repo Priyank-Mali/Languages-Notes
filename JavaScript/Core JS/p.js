4e1fe3241c1b1133c0e6d0c4362f71b18722507a
priyank <priyankmali297@gmail.com>
2024-11-13 00:47:38 +0530

    javascript

--- JavaScript/Core JS/p.js
+++ JavaScript/Core JS/p.js
@@ -1,17 +1,2 @@
-let heros = ["sdsdfs","sdfsdf","sdfsdf"]
-
-let dsf = new Array()
-
-dsf[0] = "asdas"
-dsf[1] = "asda"
-
-dsf.push(("45","458"))
-
-dsf.unshift("23")
-dsf.unshift("56")
-
-console.log(dsf)
-console.log(dsf.slice(1,3))
-
-console.log(dsf.splice(1,3))
-
+let name = Array.from("priyank")
+console.log(name)
--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,3 +1,12 @@
-let myname = ""
-        let booleanname = Boolean(myname)
-        console.log(booleanname) // true
+
+function isPrime(number){
+        for (let start=2;start<=Math.sqrt(number);start++){
+                if (number%start == 0){
+                        return false
+                } 
+                
+        }
+        return true
+}
+
+console.log([54,3,4,5,7,11,19,6,7,,4].filter(isPrime)) 
