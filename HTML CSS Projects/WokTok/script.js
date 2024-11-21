cd576e1743f1d76b365ea8c96ea8f97701b2813f
priyank <priyankmali297@gmail.com>
2024-11-22 01:15:19 +0530

    html/css projects

--- HTML CSS Projects/WokTok/script.js
+++ HTML CSS Projects/WokTok/script.js
@@ -24,17 +24,48 @@ let sidebar = document.getElementsByClassName("navbar-responsive")[0].lastElemen
 sidebar.addEventListener("click",()=>{
     if (sidebar.className == "fa-solid fa-bars"){
         document.getElementById("sidebar").style.display = "block";
+        document.getElementById("sidebar").children[0].classList.add("slide")
         sidebar.className = "fa-solid fa-xmark";
     } else {
         document.getElementById("sidebar").style.display = "none";
         sidebar.className = "fa-solid fa-bars";
+        document.getElementById("sidebar").children[0].classList.remove("slide")
 
     }
-})
+});
 
 
 // ============================================================================================
 // animation
 
-let animation = Array.from(document.getElementsByClassName("animation"))
-console.log(animation)
+// let animation = Array.from(document.getElementsByClassName("animation"))
+
+
+// ================================================================================================
+// shoppingcart
+
+function shoppingCart(){
+    let shoppingcart = document.getElementById("shoppingcarticon")
+    let cartsidebar =  document.querySelector(".cartsidebar")
+    let closecart = document.querySelector(".closecart")
+    cartsidebar.style.display = "block";
+
+    closecart.addEventListener("click",()=>{
+        cartsidebar.style.display = "none";
+    })
+    
+    return false
+}
+
+
+// ======================================================================================================
+// searchbar
+
+function searchBar(){
+    document.querySelector(".searchbar").style.display = "block"
+    document.getElementById("closesearchbar").addEventListener("click",()=>{
+        document.querySelector(".searchbar").style.display = "none"
+    })
+
+    return false
+}
