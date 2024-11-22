0a6aefeb28729d60d774c25be3e10ac8264827b1
🇵‌🇷‌🇮‌🇾‌🇦‌🇳‌🇰‌ 🇲‌🇦‌🇱‌🇮‌ <priyankmali297@gmail.com>
2024-11-22 17:37:41 +0530

    html/css projects

--- HTML CSS Projects/WokTok/script.js
+++ HTML CSS Projects/WokTok/script.js
@@ -1,3 +1,69 @@
+// animation slider
+// ===============================================================
+
+console.log(document.querySelector(".spotlight").offsetWidth)
+if(document.querySelector(".spotlight").offsetWidth>990){
+    $('.slider1').bxSlider({
+        minSlides: 6,
+        maxSlides: 4,
+        slideWidth: 400,
+        slideMargin: 2,
+        ticker: true,
+        speed : 10000,    
+    })
+    $('.slider2').bxSlider({
+        minSlides: 6,
+        maxSlides: 4,
+        slideWidth: 400,
+        slideMargin: 2,
+        ticker: true,
+        speed : 12000, 
+        autoDirection: 'prev'
+    })
+} else if(document.querySelector(".spotlight").offsetWidth<990 && document.querySelector(".spotlight").offsetWidth>700) {
+    console.log("okay")
+    $('.slider1').bxSlider({
+        minSlides: 3,
+        maxSlides: 8,
+        slideWidth: 400,
+        slideMargin: 2,
+        ticker: true,
+        speed : 10000,    
+    })
+    $('.slider2').bxSlider({
+        minSlides: 3,
+        maxSlides: 8,
+        slideWidth: 400,
+        slideMargin: 2,
+        ticker: true,
+        speed : 12000, 
+        autoDirection: 'prev'
+    })
+} else {
+    console.log("oka")
+    $('.slider1').bxSlider({
+        minSlides: 2,
+        maxSlides: 8,
+        slideWidth: 400,
+        slideMargin: 2,
+        ticker: true,
+        speed : 10000,    
+    })
+    $('.slider2').bxSlider({
+        minSlides: 2,
+        maxSlides: 8,
+        slideWidth: 400,
+        slideMargin: 4,
+        ticker: true,
+        speed : 12000, 
+        autoDirection: 'prev'
+    })
+}
+
+
+
+
+
 // slider
 // =======================================================
 
